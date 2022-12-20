addc6bc4d6e9534ff9f7a7ed88ff3549b3e0de7d
Kavin Phan <kavin@kphan.tech>
2022-12-19 22:00:40 -0500

    Create example component and also move to src/ directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
+import ExampleComponent from "../components/ExampleComponent"
+
 export default function Home() {
   return (
     <main>
       <p>Hello world!</p>
+      <ExampleComponent />
     </main>
   )
 }
