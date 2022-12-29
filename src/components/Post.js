1ebc6647c56457bb7e17af12ba87ca268cda4589
Johnathan <jonathanhas9@gmail.com>
2022-12-28 23:56:39 -0500

    add post component

---
+++ src/components/Post.js
@@ -0,0 +1,9 @@
+export default function Post() {
+  return (
+    <div>
+      <p>@Kavin</p>
+      <p>December 12th, 2022</p>
+      <p>Lorem ipsum agaegbaehbnaha</p>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
-import ExampleComponent from "../components/ExampleComponent"
+import ExampleComponent from "../components/ExampleComponent";
+import Post from "../components/Post";
 
 export default function Home() {
   return (
     <main>
-      <p>Hello world!</p>
+      <p>Hello planet!</p>
       <ExampleComponent />
+      <br />
+      <Post />
+      <br />
+      <Post />
     </main>
-  )
+  );
 }
