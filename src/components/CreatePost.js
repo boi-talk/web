29acd052b8c6e15f77d099e1f45e6cae8e3c8da9
Kevin <kevin@KevinHuynh.localdomain>
2022-12-29 13:09:46 -0500

    Added a simple text field and a button

---
+++ src/components/CreatePost.js
@@ -0,0 +1,10 @@
+export default function CreatePost () {
+    return (
+        <div>
+            <form>
+            <input type= "text" name= "text" class="talk"></input>
+            </form>
+            <button type="button">Submit</button>
+        </div>
+    )
+}
