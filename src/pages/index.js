f35b400998cabde5322eb4e8dd050feebdf9e96f
Kevin Trinh <kevintrinh388@gmail.com>
2023-01-17 01:26:49 -0500

    create protected component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,21 @@
 import ExampleComponent from '../components/ExampleComponent';
 import Post from '../components/Post';
 import CreatePost from '../components/CreatePost';
+import Protected from '../components/Protected';
 
 export default function Home() {
   return (
-    <main>
-      <p>whats happening</p>
-      <CreatePost />
-      <p>Hello planet!</p>
-      <ExampleComponent />
-      <br />
-      <Post />
-      <br />
-      <Post />
-    </main>
+    <Protected>
+      <main>
+        <p>whats happening</p>
+        <CreatePost />
+        <p>Hello planet!</p>
+        <ExampleComponent />
+        <br />
+        <Post />
+        <br />
+        <Post />
+      </main>
+    </Protected>
   );
 }
